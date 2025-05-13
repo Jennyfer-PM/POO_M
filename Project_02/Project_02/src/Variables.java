ae9de8c726d969851930548e490236f5eb610a6e
Jennyfer-PM <124049932@upq.edu.mx>
2025-05-13 08:47:17 -0600

    Se creo un Publisher

---
+++ Project_02/Project_02/src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        double numero2;
+        numero2 = 3.14; //Puede requerir colocar una D o d al final del valor
+        float numero3;
+        numero3 = 3.14159F; //Requiere colocar una F o f al final del valor
+        char caracter1 = 'A', caracter2 = 65;
+        boolean opcion = true;
+        long numero4;
+        numero4 = 987654321L; //Puede requerir L o l al final del valor
+        byte numero5 = 126;
+        short numero6 = 128;
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+        System.out.println(caracter1);
+        System.out.println(caracter2);
+        System.out.println(opcion);
+    }
+}
